666250d2142f51a59b7a14a447ac36c25b455440
Gustavo Bicalho <gusbicalho@gmail.com>
2022-04-29 05:56:15 +0000

    add instance for pairs

--- src/Deriving/On/Class.hs
+++ src/Deriving/On/Class.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE AllowAmbiguousTypes    #-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE FlexibleContexts       #-}
 {-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE KindSignatures         #-}
@@ -12,9 +14,16 @@ module Deriving.On.Class where
 
 import GHC.Records (HasField (getField))
 import GHC.TypeLits (Symbol)
+import Data.Kind (Type)
 
 class OnTarget k (target :: k) a b | k target a -> b where
   getTarget :: a -> b
 
 instance HasField field a b => OnTarget Symbol field a b where
   getTarget = getField @field
+
+instance (OnTarget Type t a b, OnTarget Type u a c) => OnTarget Type (t, u) a (b, c) where
+  getTarget a = (getTarget @_ @t a, getTarget @_ @u a)
+
+instance (OnTarget k t a b, OnTarget l u a c) => OnTarget (k, l) '(t, u) a (b, c) where
+  getTarget a = (getTarget @k @t a, getTarget @l @u a)
